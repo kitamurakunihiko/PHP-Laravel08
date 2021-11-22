c0b798b99cb59e328ed5b75e9ec501619f04d6da
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2021-11-22 12:31:07 +0000

    Laravel09の課題

---
+++ app/Http/Controllers/Admin/Laravel09.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('XXX', 
+'Admin\AAAController@bbb');
+
--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -10,4 +10,3 @@ class NewsController extends Controller
     //
 }
 
-
