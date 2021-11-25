84ae694b04831420eafa46c31a2d41e8cee8b446
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2021-11-25 05:35:46 +0000

    課題１０保存２

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="ytf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>profile</title>
+    </head>
+    <body>
+        <h1>プロフィール編集画面</h1>
+    </body>
+</html>
