d9878bb91977363fc85ee14206cfcae9f5797b18
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2021-12-10 04:03:57 +0000

    テスト

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -3,6 +3,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\News;
+//以下を追記
+use Carbon\Carbon;
+use App\History;
+
 
 class NewsController extends Controller
 {
@@ -89,6 +93,12 @@ class NewsController extends Controller
 
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
+      
+      // 以下を追記
+      $history = new History();
+      $history->news_id = $news->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
 
       return redirect('admin/news');
   }
