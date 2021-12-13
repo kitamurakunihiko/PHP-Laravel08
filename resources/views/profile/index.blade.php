47c0389f0fd0d0d9325328f559b8fb823ecfa899
kitamurakunihiko <fourthkwitht_9292@yahoo.co.jp>
2021-12-13 07:27:32 +0000

    Laravel19課題

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -10,12 +10,18 @@
                         <div class="col-md-6">
                             <div class="caption mx-auto">
                                 <div class="title p-2">
-                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                    <h1>{{ str_limit($headline->name, 50) }}</h1>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
+                            <p class="body mx-auto">{{ str_limit($headline->gender, 20) }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="body mx-auto">{{ str_limit($headline->hobby, 300) }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="body mx-auto">{{ str_limit($headline->introduction, 500) }}</p>
                         </div>
                     </div>
                 </div>
@@ -31,11 +37,17 @@
                                 <div class="date">
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
-                                <div class="title">
-                                    {{ str_limit($post->title, 150) }}
+                                <div class="name">
+                                    {{ str_limit($post->name, 50) }}
                                 </div>
-                                <div class="body mt-3">
-                                    {{ str_limit($post->body, 1500) }}
+                                <div class="gender">
+                                    {{ str_limit($post->gender, 30) }}
+                                </div>
+                                <div class="hobby">
+                                    {{ str_limit($post->hobby, 300) }}
+                                </div>
+                                <div class="introdction mt-3">
+                                    {{ str_limit($post->introduction, 1500) }}
                                 </div>
                             </div>
                         </div>
